47a6e4275431d49136c4eeb6faaacd5b3f309a6a
mghmgm <147536497+mghmgm@users.noreply.github.com>
2025-06-18 23:16:01 +0300

    fix: nginx

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -176,10 +176,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static/'
-STATIC_URL = "static/"
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+STATIC_URL = '/static/'
+STATIC_ROOT = '/usr/src/app/static/'  # Должен совпадать с Dockerfile
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/usr/src/app/media/'
 
 
 # Default primary key field type
